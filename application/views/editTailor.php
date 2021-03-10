5002c3d35cec973161800f17274b5db93e5edc4c
FRAUDLENS\Dania <dania.parvez@zigron.com>
2021-03-10 19:30:32 +0500

    edit added along columns

---
+++ application/views/editTailor.php
@@ -0,0 +1,131 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+	<link href="<?php echo base_url() ?>assets/vendor/fontawesome/css/fontawesome.min.css" rel="stylesheet">
+    <link href="<?php echo base_url() ?>assets/vendor/fontawesome/css/solid.min.css" rel="stylesheet">
+    <link href="<?php echo base_url() ?>assets/vendor/fontawesome/css/brands.min.css" rel="stylesheet">
+    <link href="<?php echo base_url() ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url() ?>assets/css/master.css" rel="stylesheet">
+    <link href="<?php echo base_url() ?>assets/vendor/chartsjs/Chart.min.css" rel="stylesheet">
+    <link href="<?php echo base_url() ?>assets/vendor/flagiconcss/css/flag-icon.min.css" rel="stylesheet">
+
+</head>
+<body>
+    <div class="wrapper">
+    <nav id="sidebar" class="">
+            <ul class="list-unstyled components text-secondary">
+                <li>
+                    <a href="<?php echo base_url() ?>index.php/dashboard/dashboardStats"><i class="fas fa-chalkboard-teacher"></i> Dashboard</a>
+                </li>
+                <li>
+                    <a href="#uielementsmenu1" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle no-caret-down"><i class="fas fa-users"></i> Customer Management</a>
+                    <ul class="collapse list-unstyled" id="uielementsmenu1">
+                        <li>
+                            <a href="<?php echo base_url()?>index.php/customer/addCustomer"><i class="fas fa-angle-right"></i> Add Customer</a>
+                        </li>
+                        <li>
+                            <a href="<?php echo base_url()?>index.php/customer/viewCustomer"><i class="fas fa-angle-right"></i>Manage Customer</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#uielementsmenu2" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle no-caret-down"><i class="fas fa-users-cog"></i> Tailor Management</a>
+                    <ul class="collapse list-unstyled" id="uielementsmenu2">
+                        <li>
+                            <a href="<?php echo base_url()?>index.php/dashboard/addTailor"><i class="fas fa-angle-right"></i> Add Tailor</a>
+                        </li>
+                        <li>
+                            <a href="<?php echo base_url()?>index.php/dashboard/viewTailor"><i class="fas fa-angle-right"></i>Manage Tailor</a>
+                        </li>
+                    </ul>
+                </li><li>
+                    <a href="#uielementsmenu3" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle no-caret-down"><i class="fas fa-tshirt"></i> Dress Management</a>
+                    <ul class="collapse list-unstyled" id="uielementsmenu3">
+                        <li>
+                            <a href="<?php echo base_url()?>index.php/dress/addDress"><i class="fas fa-angle-right"></i> Add Dress</a>
+                        </li>
+                        <li>
+                            <a href="<?php echo base_url()?>index.php/dress/viewDress"><i class="fas fa-angle-right"></i>Manage Dress</a>
+                        </li>
+                    </ul>
+                </li><li>
+                    <a href="#uielementsmenu4" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle no-caret-down"><i class="fas fa-cart-plus"></i> Order Management</a>
+                    <ul class="collapse list-unstyled" id="uielementsmenu4">
+                        <li>
+                            <a href="<?php echo base_url()?>index.php/order/addOrder"><i class="fas fa-angle-right"></i> Add Order</a>
+                        </li>
+                        <li>
+                            <a href="<?php echo base_url()?>index.php/order/viewOrder"><i class="fas fa-angle-right"></i>Manage Order</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <div id="body" class="">
+            <div class="content">
+                <div class="container">
+                    <div class="row">
+                    <div class="col-md-12">
+                       <div class="card">
+                                <div class="card-header">Edit Tailor</div>
+                                <div class="card-body">
+                                    <!-- <h5 class="card-title">Example form with inline field validations</h5> -->
+                                    <form method="post" >
+                                    <!-- class="needs-validation" method="post" novalidate="" accept-charset="utf-8" -->
+                                        <div class="form-row">
+                                            <div class="form-group col-md-6">
+                                                <label for="email">Email</label>
+                                                <input type="email" value="<?php echo $tailor_data->email ?>" class="form-control" name="email"  required/>
+                                                <small class="form-text text-muted">Enter a valid email address.</small>
+                                            </div>
+                                            <div class="form-group col-md-6">
+                                                <label for="password">Password</label>
+                                                <input type="text" class="form-control" name="password" value="<?php echo $tailor_data->password ?>" required/>
+                                                <small class="form-text text-muted">Your password must be 8-20 characters long, contain letters and numbers only.</small>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="address">Address</label>
+                                            <input type="text" class="form-control" name="address" value="<?php echo $tailor_data->address ?>" required/>
+                                        </div>
+                                        <div class="form-row">
+                                        <div class="form-group col-md-6">
+                                                <label for="name">Name</label>
+                                                <input type="text" value="<?php echo $tailor_data->name ?>" class="form-control" name="name"  required/>
+                                        </div>
+                                        <div class="form-group col-md-6">
+                                            <label for="email">Contact</label>
+                                            <input type="text" value="<?php echo $tailor_data->contact ?>" class="form-control" name="contact"  required/>
+                                            <small class="form-text text-muted">Enter a valid contact format(03xx-xxxxxxx)</small>
+                                        </div>
+                                        
+                                        </div>
+                                        <!--Hidden field for type -->
+                                        <div class="form-group col-md-6">
+                                        
+                                                <input type="text" class="form-control" name="id"  value="<?php echo $tailor_data->id ?>" />
+                                                <input type="text" class="form-control" name="type" value="<?php echo $tailor_data->type ?>" />
+                                        </div>
+                                      
+                                        <input type="submit" name="update" value='Update' class="btn btn-primary"/>
+                                        <small><?php echo @$customer_msg ?></small>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+	<script src="<?php echo base_url() ?>assets/vendor/jquery/jquery.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?php echo base_url() ?>ssets/vendor/chartsjs/Chart.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/js/dashboard-charts.js"></script>
+    <script src="<?php echo base_url() ?>assets/js/script.js"></script>
+</body>
+</html>
